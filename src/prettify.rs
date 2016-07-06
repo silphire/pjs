35137062a873d0f554cbe2062af10fc762d54e02
Yuki Mitsui <silphire@gmail.com>
2016-07-06 12:32:13 +0000

    make compilable

--- src/prettify.rs
+++ src/prettify.rs
@@ -4,15 +4,66 @@ extern crate getopts;
 
 use std::error::Error;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::path::Path;
 
 fn prettify(file: &mut File) {
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
-        Err(_) => panic!(""), 
+        Err(_) => panic!("Cannot read from the file "), 
         Ok(_) => print!(""),
     }
+    let pos = 0;
+    println!("{}", parse_json(&contents, pos).as_str());
+}
+
+// JSON-text = ws value ws
+fn parse_json(json: &String, index: i32) -> String {
+    let mut prettified = String::new();
+    return prettified;
+}
+
+// ws = *(%x20 / %x09 / %x0A / %x0D)
+fn parse_whitespace(json: &String, index: i32) -> String {
+    let prettified = String::new();
+    return prettified;
+}
+
+// value = false / null / true / object / array / number / string
+fn parse_value(json: &String, index: i32) -> String {
+    let prettified = String::new();
+    return prettified;
+}
+
+// object = '{' [ member *( ',' member ) ] '}'
+fn parse_object(json: &String, index: i32) -> String {
+    let prettified = String::new();
+    return prettified;
+}
+
+// member = string ':' value
+fn parse_member(json: &String, index: &i32) -> String {
+    let prettified = String::new();
+    return prettified;
+}
+
+// array = '[' [ value *(':' value) ] ']'
+fn parse_array(json: &String, index: i32) -> String {
+    let prettified = String::new();
+    return prettified;
+}
+
+// number
+fn parse_number(json: &String, index: i32) -> String {
+    let prettified = String::new();
+    return prettified;
+}
+
+// string = '"' *char '"'
+fn parse_string(json: &String, index: i32) -> String {
+    let prettified = String::new();
+    return prettified;
 }
 
 pub fn uumain(args: Vec<String>) -> i32 {
@@ -33,8 +84,8 @@ pub fn uumain(args: Vec<String>) -> i32 {
 	return 0;
     }
 
-    for file in matches.free {
-	let path = Path::new(&file);
+    for filename in matches.free {
+	let path = Path::new(&filename);
 	let mut file = match File::open(&path) {
 	    Err(why) => {
 		println!("Cannot open {} {}", path.display(), Error::description(&why));
